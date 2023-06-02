ff767f56ba8c9045ecd5ad6c4ad3bf315fd6c21b
Noemi <45180344+unflxw@users.noreply.github.com>
2023-06-02 12:35:15 +0200

    Allow granular configuration beyond frameworks
    This allows us to define different configurations for different
    subsets of a framework's applications (`react/cra` and `react/vite`)
    or potentially overriding the configuration for one specific version.
    
    It also explicitly specifies which packages to link, rather than
    relying on the framework folder name matching the name of an
    `@appsignal/` package.
    
    Finally, it changes the clean command so that it removes the specific
    folder that the app uses for the build, as described in the config
    for that app, instead of always removing the `build` and `dist`
    folders.

--- support/helpers.rb
+++ support/helpers.rb
@@ -5,42 +5,65 @@ require "pry"
 require "yaml"
 require "webrick"
 
-FRAMEWORKS = {
-  "react" => {
+CONFIGS = {
+  "react/cra" => {
     :root => "build",
     :build_dir => "build",
     :js_dir => "static/js",
+    :packages => [
+      "@appsignal/react"
+    ],
+  },
+  "react/vite" => {
+    :root => "dist",
+    :build_dir => "dist",
+    :js_dir => "assets",
+    :packages => [
+      "@appsignal/react"
+    ],
   },
   "vue" => {
     :root => "dist",
     :build_dir => "dist",
-    :js_dir => "js"
+    :js_dir => "js",
+    :packages => [
+      "@appsignal/vue"
+    ],
   },
   "angular" => {
     :root => "dist/app",
     :build_dir => "dist",
-    :js_dir => "app"
+    :js_dir => "app",
+    :packages => [
+      "@appsignal/angular"
+    ],
   },
   "stimulus" => {
     :root => "public",
     :build_dir => "public",
-    :js_dir => ""
+    :js_dir => "",
+    :packages => [
+      "@appsignal/stimulus"
+    ],
   }
 }
 
 PACKAGE_MANAGER = "yarn"
 
 def all_apps
-  FRAMEWORKS.map do |language, config|
-    Dir["frameworks/#{language}/*"].sort
-  end.flatten.map do |path|
+  Dir["frameworks/*/*"].sort.map do |path|
     path.gsub("frameworks/", "")
   end
 end
 
-def framework_config(app)
-  framework = app.split("/").first
-  FRAMEWORKS[framework] or raise "#{framework} not configured"
+def app_config(app)
+  config = CONFIGS.find do |key, value|
+    app.start_with?(key)
+  end
+
+  raise "#{app} not configured" unless config
+
+  config[1]
 end
 
 def get_app
@@ -82,7 +105,7 @@ def write_appsignal_config(app, frontend_key, revision, uri)
 end
 
 def upload_sourcemaps(app, uri, revision, push_api_key)
-  config = framework_config(app)
+  config = app_config(app)
   base_path = "frameworks/#{app}/#{config[:build_dir]}/#{config[:js_dir]}/"
   Dir["#{base_path}*.js"].each do |path|
     filename = path.gsub(base_path, "")
@@ -116,42 +139,42 @@ def run_command(command)
 end
 
 def run_install(app)
-  run_command "cd frameworks/#{app} && #{PACKAGE_MANAGER} install --no-fund --no-audit"
+  run_command "cd frameworks/#{app} && #{PACKAGE_MANAGER} install --force --no-fund --no-audit"
 end
 
 def run_build(app)
   run_command "cd frameworks/#{app} && #{PACKAGE_MANAGER} run build"
 end
 
+def app_packages(app)
+  config = app_config(app)
+
+  [
+    "@appsignal/types",
+    "@appsignal/core",
+    "@appsignal/javascript",
+  ] + config[:packages]
+end
+
 def run_link
   all_apps.each do |app|
-    framework = app.split("/").first
-    packages = [
-      "@appsignal/types",
-      "@appsignal/core",
-      "@appsignal/javascript",
-      "@appsignal/#{framework}",
-    ]
+    packages = app_packages(app)
+
     run_command "cd frameworks/#{app} && #{PACKAGE_MANAGER} link #{packages.join(" ")}"
   end
 end
 
 def run_unlink
   all_apps.each do |app|
-    framework = app.split("/").first
-    packages = [
-      "@appsignal/types",
-      "@appsignal/core",
-      "@appsignal/javascript",
-      "@appsignal/#{framework}",
-    ]
+    packages = app_packages(app)
+
     run_command "cd frameworks/#{app} && #{PACKAGE_MANAGER} unlink #{packages.join(" ")}"
   end
 end
 
 def run_webserver(app, port=5001)
   puts "Starting webserver for #{app}"
-  config = framework_config(app)
+  config = app_config(app)
   WEBrick::HTTPServer.new(
     :Port => port,
     :DocumentRoot => "frameworks/#{app}/#{config[:root]}"
