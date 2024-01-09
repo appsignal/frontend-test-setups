12e0ac3e87b88847d944a113cd3b27b9d70d1830
Tom de Bruijn <tom@tomdebruijn.com>
2024-01-09 14:00:20 +0100

    Add Vue version 3 app using composition
    This is the new way that Vue 3 apps are recommended to be built.
    This will report a component name using the changes in
    https://github.com/appsignal/appsignal-javascript/pull/614
    
    Keep the older class component method around in the renamed app
    directory.

--- frameworks/vue/3/src/main.ts
+++ frameworks/vue/3/src/main.ts
@@ -1,9 +1,14 @@
-import appsignal from "./appsignal"
+import './assets/main.css'
+
+import appsignal from "./appsignal";
 import { errorHandler } from "@appsignal/vue";
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 const app = createApp(App)
-app.config.errorHandler = errorHandler(appsignal, app)
+app.config.errorHandler = errorHandler(appsignal, app);
+app.use(router)
+
 app.mount('#app')
